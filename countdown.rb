52bdedd412106d08d90fe579475be062febfaa37
Dominique De León <dominique.a.deleon@gmail.com>
2020-03-26 17:02:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,3 @@
-require "pry"
-# and uses a while loop to countdown from that integer to 0, 
-# outputting "#{number} SECOND(S)!" in each iteration of the loop. 
-#The method should return "HAPPY NEW YEAR!" after the loop finishes. 
-
-
 def countdown(int)
   
   while (int > 0) 
