b0f2e15b49da9e337142c0bf6474f1b02e0823fe
Dominique De León <dominique.a.deleon@gmail.com>
2020-03-26 16:39:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,10 @@
 
-
-# Write a method that takes in an integer argument 
 # and uses a while loop to countdown from that integer to 0, 
 # outputting "#{number} SECOND(S)!" in each iteration of the loop. 
 #The method should return "HAPPY NEW YEAR!" after the loop finishes. 
 
 
-def countdown(int)
+def countdown(int) # 1-
   
   while (condition)
     "#{number} SECOND(S)!"
