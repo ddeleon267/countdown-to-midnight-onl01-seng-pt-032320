59ce49252e3eecad56e320881c15dbf06cb82e3a
Dominique De León <dominique.a.deleon@gmail.com>
2020-03-26 16:45:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 # and uses a while loop to countdown from that integer to 0, 
 # outputting "#{number} SECOND(S)!" in each iteration of the loop. 
 #The method should return "HAPPY NEW YEAR!" after the loop finishes. 
